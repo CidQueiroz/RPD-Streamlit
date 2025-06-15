057242ec88ae7a0ff98a16f09384dd3141c0524d
Cid Queiroz <cydy.queiroz@gmail.com>
2025-06-15 14:01:58 -0300

    removendo indexador

--- teste_rpd.py
+++ teste_rpd.py
@@ -247,7 +247,7 @@ elif opcao == "Visualizar respostas":
     if df_respostas.empty:
         st.info("Nenhuma resposta registrada ainda.")
     else:
-        st.dataframe(df_respostas)
+        st.dataframe(df_respostas, use_container_width=True, hide_index=True)
         csv = df_respostas.to_csv(index=False).encode("utf-8")
         st.download_button(
             label="Baixar todas as respostas em CSV",
